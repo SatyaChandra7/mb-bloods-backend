c1a764a2d6be1391fd8613441942ef76bbd9bc7a
SatyaChandra7 <satyachandra722@gmail.com>
2026-04-01 00:08:03 +0530

    Configure Vercel deployment

--- server.js
+++ server.js
@@ -80,6 +80,9 @@ app.get('/api/donors/count', async (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+module.exports = app;
